08ec630203268ae9dba1aff4d07b5e73a4b1fdf0
Vincent Kwok <vkwok22@gmail.com>
2018-07-02 19:20:24 -0500

    add figaro unirest and bars controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/bars" => "bars#index"
+  get "/bars/:id" => "bars#show"
 
 end
